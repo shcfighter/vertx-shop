d871ba210a7065c1893e00a85e21340eb47acc4f
shc_fighter <shc_fighter@163.com>
2018-12-24 15:36:51 +0800

    package

---
+++ shop-common/src/main/java/com/ecit/common/auth/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Indicates that this module contains classes that need to be generated / processed.
+ */
+@ModuleGen(name = "vertx-shop", groupPackage = "com.ecit")
+
+package com.ecit.common.auth;
+
+import io.vertx.codegen.annotations.ModuleGen;
