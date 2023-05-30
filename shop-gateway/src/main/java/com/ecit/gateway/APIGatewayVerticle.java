c391fb3773338d90637ba748edf0c9d397ae6587
shcfighter <1224288531@qq.com>
2023-05-30 19:44:45 +0800

    Update APIGatewayVerticle.java

--- shop-gateway/src/main/java/com/ecit/gateway/APIGatewayVerticle.java
+++ shop-gateway/src/main/java/com/ecit/gateway/APIGatewayVerticle.java
@@ -23,8 +23,8 @@ import io.vertx.reactivex.servicediscovery.ServiceDiscovery;
 import io.vertx.servicediscovery.Record;
 import io.vertx.servicediscovery.types.HttpEndpoint;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Objects;
