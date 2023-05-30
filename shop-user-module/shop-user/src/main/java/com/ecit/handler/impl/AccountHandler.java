8110fd8c9eb212cb720ab39f5be7281fbc31576a
shcfighter <1224288531@qq.com>
2023-05-30 19:37:12 +0800

    Update AccountHandler.java

--- shop-user-module/shop-user/src/main/java/com/ecit/handler/impl/AccountHandler.java
+++ shop-user-module/shop-user/src/main/java/com/ecit/handler/impl/AccountHandler.java
@@ -20,8 +20,8 @@ import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.sqlclient.Tuple;
 import io.vertx.serviceproxy.ServiceProxyBuilder;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
 import java.util.Objects;
