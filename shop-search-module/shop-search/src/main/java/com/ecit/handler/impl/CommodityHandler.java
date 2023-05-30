9a7aba895201c18602761a3296b98ec3e46cf853
shcfighter <1224288531@qq.com>
2023-05-30 19:39:19 +0800

    Update CommodityHandler.java

--- shop-search-module/shop-search/src/main/java/com/ecit/handler/impl/CommodityHandler.java
+++ shop-search-module/shop-search/src/main/java/com/ecit/handler/impl/CommodityHandler.java
@@ -15,8 +15,8 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.sqlclient.Tuple;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
