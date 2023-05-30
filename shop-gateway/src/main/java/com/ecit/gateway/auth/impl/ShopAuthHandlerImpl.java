b1dcf917b6a95e283ca60af80958e3eac07e7744
shcfighter <1224288531@qq.com>
2023-05-30 19:45:03 +0800

    Update ShopAuthHandlerImpl.java

--- shop-gateway/src/main/java/com/ecit/gateway/auth/impl/ShopAuthHandlerImpl.java
+++ shop-gateway/src/main/java/com/ecit/gateway/auth/impl/ShopAuthHandlerImpl.java
@@ -17,8 +17,8 @@ import io.vertx.reactivex.core.Promise;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.sqlclient.Tuple;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
